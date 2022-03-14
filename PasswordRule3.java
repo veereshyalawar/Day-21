806029b272656ff5d5c5cdae2a1aff8d2ecebe81
veeresh <veereshyalawar2@gmail.com>
2022-03-14 21:31:42 +0530

    added uc8

---
+++ PasswordRule3.java
@@ -0,0 +1,57 @@
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordRule3 {
+
+	public static void main(String[] args) {
+		/*
+		 * 
+		 * ##################################################### 
+		 * 1.Taking user input of password
+		 * 3.calling method to print to print results  
+		 * 2.Here we created a method which checks whether the entered password is maches or not
+		 * and at least contain an upper case and one numeric or not
+		 * and have only one special character or not
+		 * #####################################################
+		 *
+		 */
+
+		/*
+		 * 
+		 * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 
+		 * 1.Taking user input of password
+		 * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+		 *
+		 */
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Password must contain atleast 8 character and atleast one upper case and one numeric "
+				+ "and only one special character");
+		System.out.println("Enter the password: ");
+		String Password = sc.nextLine();
+
+		/*
+		 * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 
+		 * 3.calling method to print
+		 * the result $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+		 *
+		 */
+		System.out.println(password(Password));
+
+	}
+
+	/*
+	 * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 
+	 * 2.Here we created a method which checks whether the entered password is matches or not
+	 *   and at least contain an upper case and one numeric or not
+	 *   and have only one special character or not
+	 * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+	 *
+	 */
+	public static boolean password(String password) {
+
+		return password.matches("^(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[a-z]+).{8,}(?=.*[%#@&!]+).{1}$");
+
+	}
+}
